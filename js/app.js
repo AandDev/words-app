afba5c042fe5cb89c8361bf9db66433f7390fc78
Andres <andha037@gmail.com>
2020-09-22 19:15:52 -0300

    Improving functions and layout

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const wordsBtn = document.querySelectorAll('.word-btn');
+const sounds = document.querySelectorAll('.sound');
 
 const vocalesSample = [
     ['Avión', 'Aire', 'Agua'],
@@ -9,50 +10,70 @@ const vocalesSample = [
 ];
 const colors = ['#FFD600', '#2bdd52', '#2EADF4', '#CD2EF4', '#F42E2E'];
 
-/*
-wordsBtn.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        const examplesBox = document.createElement('div');
-        const newWords = `
-        <div class="exp-item" style="background-color: ${colors[index]}">
-            <button class="exp-btn" id="exp-btn">
-                ${vocalesSample[index][0]}
-            </button>
-            <audio class="sound-2" src="../sounds/owl.mp3"></audio>
-        </div>
-        <div class="exp-item" style="background-color: ${colors[index]}">
-            <button class="exp-btn">
-                ${vocalesSample[index][1]}
-            </button>
-            <audio class="sound-2" src="../sounds/pig.mp3"></audio>
-        </div>
-        <div class="exp-item" style="background-color: ${colors[index]}">
-            <button class="exp-btn">
-                ${vocalesSample[index][2]}
-            </button>
-            <audio class="sound-2" src="../sounds/elephant.mp3"></audio>
-        </div>
-        `;
 
-        examplesBox.classList.add('examples');
-        examplesBox.innerHTML = newWords;
+function createExamples(item, color, sound) {
+    
+    const examplesBox = document.createElement('div');
+    const newWords = `
+    <div class="exp-item" style="background-color: ${color}">
+        <button class="exp-btn" id="exp-btn">
+            ${item[0]}
+        </button>
+        <audio class="sound-2" src="../sounds/owl.mp3"></audio>
+    </div>
+    <div class="exp-item" style="background-color: ${color}">
+        <button class="exp-btn">
+            ${item[1]}
+        </button>
+        <audio class="sound-2" src="../sounds/pig.mp3"></audio>
+    </div>
+    <div class="exp-item" style="background-color: ${color}">
+        <button class="exp-btn">
+            ${item[2]}
+        </button>
+        <audio class="sound-2" src="../sounds/elephant.mp3"></audio>
+    </div>
+    `;
+    console.log(color);
+    examplesBox.classList.add('examples');
+    examplesBox.innerHTML = newWords;
 
-        const wordWrap = document.querySelector('.content');
-        const sounds = document.querySelectorAll('.sound');
+    const wordWrap = document.querySelector('.content');
 
-        //sounds[index].currentTime = 0;
-        //sounds[index].play();
+    sound.currentTime = 0;
+    sound.play();
 
-        if(wordWrap.hasChildNodes){
-            wordWrap.removeChild(wordWrap.lastChild);
-            wordWrap.appendChild(examplesBox);
-        } else {
-            wordWrap.appendChild(examplesBox);
-        }
-    })
+    if(wordWrap.hasChildNodes){
+        wordWrap.removeChild(wordWrap.lastChild);
+        wordWrap.appendChild(examplesBox);
+    } else {
+        wordWrap.appendChild(examplesBox);
+    }
+    
+}
+
+wordsBtn.forEach((btn, index) => btn.addEventListener('click', () => createExamples(vocalesSample[index], colors[index], sounds[index])));
+
+// ---------- Sonidos de palabras ---------- //
+
+document.addEventListener('click',(e) => {
+
+    if(e.target.classList == 'exp-btn'){
+        const sound = e.target.nextElementSibling;
+
+        sound.currentTime = 0;
+        sound.play();
+    }
 });
-*/
 
+export {createExamples, colors, sounds};
+
+
+
+
+// ========== Old function ========== //
+
+/*
 const createExamples = (btn, index) => {
     btn.addEventListener('click', () => {
         const examplesBox = document.createElement('div');
@@ -95,28 +116,5 @@ const createExamples = (btn, index) => {
     })
 }
 
-
 wordsBtn.forEach(createExamples);
-
-
-// ---------- Sonidos de palabras ---------- //
-
-document.addEventListener('click',(e) => {
-
-    if(e.target.classList == 'exp-btn'){
-        const sound = e.target.nextElementSibling;
-
-        sound.currentTime = 0;
-        sound.play();
-    }
-});
-
-
-
-/*
-function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
 */
-
-export {createExamples};
--- js/consonantes.js
+++ js/consonantes.js
@@ -1,14 +1,14 @@
-import {createExamples} from './app.js'; 
+import {createExamples, colors, sounds} from './app.js'; 
 
 const wordsBtn = document.querySelectorAll('.word-btn');
 
-const vocalesSample = [
+const consonantesSample = [
     ['Mamá', 'Mesa', 'Martes'],
     ['Papá', 'Pala', 'Pato'],
     ['Lata', 'Letra', 'Lila'],
     ['Taza', 'Torre', 'Tela'],
     ['Sábado', 'Suelo', 'Sanar']
 ];
-const colors = ['#FFD600', '#2bdd52', '#2EADF4', '#CD2EF4', '#F42E2E'];
 
-wordsBtn.forEach(createExamples);
+
+wordsBtn.forEach((btn, index) => btn.addEventListener('click', () => createExamples(consonantesSample[index], colors[index], sounds[index])));
