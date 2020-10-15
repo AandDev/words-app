3c67214c2586f97f6788d2b73438c64179b712af
Andres <andha037@gmail.com>
2020-10-15 17:30:20 -0300

    Adding animated background

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-import {vocalesSample, vocalesSounds} from './sampleWords.js'
+import {colors1, vocalesSample, vocalesSounds} from './sampleWords.js'
 
 const wordsBtn = document.querySelectorAll('.word-btn');
 const sounds = document.querySelectorAll('.sound');
+const homeBg = document.querySelector('.home');
+const vocalesBg = document.querySelector('.vocales');
+const consonantesBg = document.querySelector('.consonantes');
 
 const colors = ['#FFD600', '#2bdd52', '#2EADF4', '#CD2EF4', '#F42E2E'];
 
@@ -73,5 +76,39 @@ document.addEventListener('click',(e) => {
         sound.play();
     }
 });
+
+
+
+// ---------- Background animado ---------- //
+
+const createSquare = (container, color) => {
+    const square = document.createElement('span');
+    let randomColor;
+
+    if(typeof color === 'object') {
+        randomColor = color[Math.floor(Math.random() * color.length)];
+    } else {
+        randomColor = color;
+    }
+
+    square.style.width = '80px';
+    square.style.height = '80px';
+    square.style.borderRadius = '7px';
+    square.style.top = Math.random() * innerHeight + 'px';
+    square.style.left = Math.random() * innerWidth + 'px';
+    square.style.backgroundColor = randomColor;
+
+    container.appendChild(square);
+
+    setTimeout( () => {
+        square.remove();
+    }, 5000);
+};
+
+setInterval(() => {createSquare(homeBg, colors)}, 800);
+setInterval(() => {createSquare(vocalesBg, colors[2])}, 800);
+setInterval(() => {createSquare(consonantesBg, colors[1])}, 800);
+
+
 
 export {createExamples, colors, sounds};
